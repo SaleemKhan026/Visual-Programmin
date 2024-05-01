bcfa6bd81990bb3f9bf2495deea2e58607fe3d3f
SaleemKhan026 <168603676+SaleemKhan026@users.noreply.github.com>
2024-04-30 23:18:50 -0700

    mid

---
+++ 07-Lists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a list of strings......
+            List<int> numbers = new List<int>();
+            //Adding elements to list......
+            numbers.Add(10);
+            numbers.Add(20);
+            numbers.Add(30);
+            numbers.Remove(20);
+            bool NumCon = numbers.Contains(10);
+            Console.WriteLine("20 number Removed ...." +numbers.Remove(20));
+            Console.WriteLine("List Contains (10)= " + NumCon);
+            Console.WriteLine($"List has Numbers");
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            
+            
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 11-Enums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Enums
+{
+    class Program
+    {
+    //    // Decleration of Enums.......
+    //    enums Days
+    //    {
+    //        Sunday,
+    //        Monday, 
+    //        Tuesday, 
+    //        Wednesday,
+    //        Thursday, 
+    //        Friday,
+    //        Saturday,
+    //    }
+
+
+        static void Main(string[] args)
+        {
+             
+
+        }
+    }
+}
