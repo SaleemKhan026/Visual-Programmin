aa353759bb48640ecb583c00356735d7a88ec94f
SaleemKhan026 <168603676+SaleemKhan026@users.noreply.github.com>
2024-05-01 19:15:47 -0700

    Final

---
+++ Rich TextBox/Rich TextBox/Rich TextBox/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Rich_TextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnCut = new System.Windows.Forms.Button();
+            this.btnUnderlined = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(799, 389);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.BackgroundImage = global::Rich_TextBox.Properties.Resources.icons8_copy_24;
+            this.btnCopy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCopy.Location = new System.Drawing.Point(386, 2);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(36, 30);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnResize
+            // 
+            this.btnResize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnResize.Location = new System.Drawing.Point(571, 2);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(36, 30);
+            this.btnResize.TabIndex = 6;
+            this.btnResize.Text = "Font";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.BackgroundImage = global::Rich_TextBox.Properties.Resources.icons8_paste_24;
+            this.btnPaste.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnPaste.Location = new System.Drawing.Point(449, 2);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(36, 30);
+            this.btnPaste.TabIndex = 5;
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnCut
+            // 
+            this.btnCut.BackgroundImage = global::Rich_TextBox.Properties.Resources.icons8_cut_24;
+            this.btnCut.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCut.Location = new System.Drawing.Point(510, 2);
+            this.btnCut.Name = "btnCut";
+            this.btnCut.Size = new System.Drawing.Size(36, 30);
+            this.btnCut.TabIndex = 4;
+            this.btnCut.UseVisualStyleBackColor = true;
+            this.btnCut.Click += new System.EventHandler(this.btnCut_Click);
+            // 
+            // btnUnderlined
+            // 
+            this.btnUnderlined.BackgroundImage = global::Rich_TextBox.Properties.Resources.icons8_underline_24;
+            this.btnUnderlined.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnUnderlined.Location = new System.Drawing.Point(329, 2);
+            this.btnUnderlined.Name = "btnUnderlined";
+            this.btnUnderlined.Size = new System.Drawing.Size(36, 30);
+            this.btnUnderlined.TabIndex = 3;
+            this.btnUnderlined.UseVisualStyleBackColor = true;
+            this.btnUnderlined.Click += new System.EventHandler(this.btnUnderlined_Click);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.BackgroundImage = global::Rich_TextBox.Properties.Resources.icons8_italic_24;
+            this.btnItalic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnItalic.Location = new System.Drawing.Point(270, 2);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(37, 30);
+            this.btnItalic.TabIndex = 2;
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click_1);
+            // 
+            // btnBold
+            // 
+            this.btnBold.BackgroundImage = global::Rich_TextBox.Properties.Resources.icons8_bold_24;
+            this.btnBold.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnBold.Location = new System.Drawing.Point(213, 2);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(36, 30);
+            this.btnBold.TabIndex = 1;
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.Location = new System.Drawing.Point(0, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 33);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "New";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(63, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Location = new System.Drawing.Point(136, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 33);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Location = new System.Drawing.Point(636, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(39, 30);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnResize);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnCut);
+            this.Controls.Add(this.btnUnderlined);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.Button btnUnderlined;
+        private System.Windows.Forms.Button btnCut;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Rich TextBox/Rich TextBox/Rich TextBox/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rich_TextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            ToggleFontStyle(FontStyle.Bold);
+        }
+
+        
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog.Color;
+            }
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog.Font;
+            }
+        }
+
+        private void ToggleFontStyle(FontStyle style)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                FontStyle currentStyle = richTextBox1.SelectionFont.Style;
+                FontStyle newStyle = currentStyle ^ style;
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, newStyle);
+            }
+        }
+
+        private void btnItalic_Click_1(object sender, EventArgs e)
+        {
+            ToggleFontStyle(FontStyle.Italic);
+        }
+
+        private void btnUnderlined_Click(object sender, EventArgs e)
+        {
+            ToggleFontStyle(FontStyle.Underline);
+        }
+
+        private void btnCut_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            FontDialog fd= new FontDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fd.Font;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+ColorDialog colorDialog = new ColorDialog();
+if (colorDialog.ShowDialog() == DialogResult.OK)
+{
+    richTextBox1.SelectionColor = colorDialog.Color;
+}
+        }
+
+       
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            {
+                richTextBox1.Clear();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    sw.Write(richTextBox1.Text);
+                }
+            }
+            }
+    }
+}
